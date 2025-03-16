4196494423fb455bf0ecd3cbd2bcc670683c9c42
User <User@DESKTOP-EAFK00E>
2025-03-16 21:58:00 +0200

    Add project files.

---
+++ Agroculture/Models/Soil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroculture.Models
+{
+    class Soil
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double DefaultN { get; set; }
+        public double DefaultP2O5 { get; set; }
+        public double DefaultK2O { get; set; }
+    }
+}
