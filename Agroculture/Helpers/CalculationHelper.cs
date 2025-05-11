b712babdb48adfdf5d5d5ab3d3e8338cf92ec112
Max Korman <mkorman359@gmail.com>
2025-05-11 19:03:23 +0300

    Helpers and models prettier code

--- Agroculture/Helpers/CalculationHelper.cs
+++ Agroculture/Helpers/CalculationHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Agroculture.Helpers
+﻿namespace Agroculture.Helpers
 {
     public static class CalculationHelper
     {
